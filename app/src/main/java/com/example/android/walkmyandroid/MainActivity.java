7401717c07e945dde1441c5d3d4e25b84ca1008d
Kevin Ripley <kevin.r.ripley.kr@gmail.com>
2020-04-13 13:21:22 -0600

    added dependencies for androidx to be used

--- app/src/main/java/com/example/android/walkmyandroid/MainActivity.java
+++ app/src/main/java/com/example/android/walkmyandroid/MainActivity.java
@@ -15,8 +15,9 @@
  */
 package com.example.android.walkmyandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
